39a94df289b8490197410906addd3d3c0efd7ad2
LAPTOP-QD16HSP6\Justin Stolk <justin.stolk@student.hku.nl>
2021-09-10 12:07:54 +0200

    Wauw, zulke goede scripts

---
+++ Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    const float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+
+    }
+
+}
