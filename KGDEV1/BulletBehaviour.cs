fa9f917b5dcee8d01d423f5391b304dcf16c0006
Andries <andriesdg199@gmail.com>
2021-09-10 12:05:25 +0200

    Added Scripts from Yesterday

---
+++ KGDEV1/BulletBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    const float BULLET_SPEED = 25f;
+    public float currentTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(transform.position, transform.forward, out hitInfo, BULLET_SPEED * Time.deltaTime))
+            {
+                IDamagable damagable = hitInfo.collider.gameObject.GetComponent<IDamagable>();
+                if (damagable != null)
+                {
+                    damagable.TakeDamage(1);
+                }
+
+
+                //for (int j = 0; j < enemies.Count; ++j)
+                //{
+                //    if (enemies[j] == hitInfo.collider.gameObject)
+                //    {
+                //        enemyHealth[j] -= 1;
+                //        if (enemyHealth[j] <= 0)
+                //        {
+                //            enemies.RemoveAt(j);
+                //            enemyHealth.RemoveAt(j);
+                //        }
+                //        break;
+                //    }
+                //}
+
+                GameObject.Destroy(this);
+            }
+
+            else
+            {
+                currentTimer -= Time.deltaTime;
+                if (currentTimer <= 0)
+                {
+                    GameObject.Destroy(this);
+                }
+                transform.Translate(transform.forward * BULLET_SPEED * Time.deltaTime, Space.World);
+            }
+    }
+}
---
+++ KGDEV1/ShootyGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootyGame : MonoBehaviour
+{
+    private static ShootyGame _instance;
+    public static ShootyGame Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    const int ENEMY_HEALTH = 100;
+
+    public GameObject enemyPrefab;
+    public GameObject playerInstance;
+
+    private List<GameObject> enemies = new List<GameObject>();
+    private List<int> enemyHealth = new List<int>();
+
+    private void Start()
+    {
+        // Spawn some enemies
+        for (int i = 0; i < 10; ++i)
+        {
+            GameObject enemy = GameObject.Instantiate(enemyPrefab);
+            enemy.transform.position = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+            enemy.GetComponent<enemy>().currentHealth = ENEMY_HEALTH;
+            enemies.Add(enemy);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
